ce4736794759e0462f78f1254e1eecb9637fa2bb
tubagus69 <tubagusputra6969@gmail.com>
2020-02-27 16:53:12 +0700

    Finish

--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -2,13 +2,32 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import id.putraprima.skorbola.model.Match;
 
 public class ScorerActivity extends AppCompatActivity {
+    public static final String ADD_KEY = "add";
+    public static final String TIME_KEY = "time";
+    EditText scorer,time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+        scorer = findViewById(R.id.et_scorer);
+        time = findViewById(R.id.et_time);
+    }
+
+    public void submitScorer(View view) {
+        Intent intent = new Intent();
+        intent.putExtra(ADD_KEY, scorer.getText().toString());
+        intent.putExtra(TIME_KEY, time.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
